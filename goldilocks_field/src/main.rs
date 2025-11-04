2adc5fe2f0089a2826bb1562056cb991ee000ed6
CarsonStewart777 <carsonstewart161803@gmail.com>
2025-11-04 18:59:50 -0500

    addition arithmetic added + working.

--- goldilocks_field/src/main.rs
+++ goldilocks_field/src/main.rs
@@ -1,3 +1,43 @@
+// define Goldilocks Prime
+pub const GOLDILOCKS_P: u64 = 0xFFFFFFFF00000001;
+
+// Create Field Element struct
+
+#[derive(Debug)]
+pub struct FieldElement(pub u64);
+
+// Constructor to ensure that the Element is within the Prime Field
+
+impl FieldElement {
+    pub fn new(value: u64) -> Self {
+        Self(value % GOLDILOCKS_P)
+    }
+}
+
+// Addition in the field
+
+use std::ops::Add;
+
+impl Add for FieldElement {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        let sum = self.0 + other.0;
+
+        // wrap around overflow
+
+        let reduced = if sum >= GOLDILOCKS_P {
+            sum - GOLDILOCKS_P
+        } else {
+            sum
+        };
+        FieldElement(reduced)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //test wrapping
+    let x = FieldElement::new(GOLDILOCKS_P);
+    let y = FieldElement::new(9);
+    let result = x + y;
+    println!("Result {:?}", result);
 }
---
+++ goldilocks_field/target/debug/deps/goldilocks_field.d
@@ -0,0 +1,5 @@
+C:\Users\Carso\goldilocks-field\goldilocks_field\target\debug\deps\goldilocks_field.exe: src\main.rs
+
+C:\Users\Carso\goldilocks-field\goldilocks_field\target\debug\deps\goldilocks_field.d: src\main.rs
+
+src\main.rs:
