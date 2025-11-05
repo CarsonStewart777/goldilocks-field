4d653393b50fb835e1a9e4f2de8e6c71f5ccd670
CarsonStewart777 <carsonstewart161803@gmail.com>
2025-11-05 03:16:20 -0500

    Remove target/ directory from Git

--- goldilocks_field/target/debug/deps/goldilocks_field.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Carso\goldilocks-field\goldilocks_field\target\debug\deps\goldilocks_field.exe: src\main.rs
-
-C:\Users\Carso\goldilocks-field\goldilocks_field\target\debug\deps\goldilocks_field.d: src\main.rs
-
-src\main.rs:
--- goldilocks_field/target/debug/goldilocks_field.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Carso\goldilocks-field\goldilocks_field\target\debug\goldilocks_field.exe: C:\Users\Carso\goldilocks-field\goldilocks_field\src\main.rs
